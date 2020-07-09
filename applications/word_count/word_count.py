f03bbf694f31a460ad4cb08ec789292503a9bf1e
Nathan <nateyl.b@gmail.com>
2020-07-08 20:20:52 -0700

    word count pass

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,5 +1,29 @@
+import re
 def word_count(s):
     # Your code here
+    counts={}
+    words = s.split()
+    for index, word in enumerate(words):
+        only_alpha = ""
+        for char in word:
+            if ord(char.lower()) >= 97 and ord(char.lower()) <= 122 or ord(char) == 39:
+                only_alpha += char.lower()
+        words[index] = only_alpha
+    if len(words) > 0:
+        if words[0] == '':
+            return counts
+    def getCount(words):
+        if len(words) == 0:
+            return
+        elif words[0] in counts:
+            counts[words[0]] += 1
+            return getCount(words[1:])
+        else:
+            counts[words[0]] = 1
+            return getCount(words[1:])
+    getCount(words)
+    print(counts,"counts")
+    return counts
 
 
 

283642ddbb5682a2a2207e0dc18fc2b4d7ce54ed
Nathan <nateyl.b@gmail.com>
2020-07-09 18:22:37 -0700

    markov done

--- applications/markov/markov.py
+++ applications/markov/markov.py
@@ -1,13 +1,48 @@
 import random
 
 # Read in all the words in one go
-with open("input.txt") as f:
+with open("applications/markov/input.txt") as f:
     words = f.read()
+
+words = words.split()
 
 # TODO: analyze which words can follow other words
 # Your code here
-
-
+markov_dict = {}
+for index,word in enumerate(words):
+    if index == len(words)-1:
+        break
+    elif word not in markov_dict:
+        markov_dict[word] = []
+    markov_dict[word].append(words[index+1])
 # TODO: construct 5 random sentences
 # Your code here
 
+def generate_sentence():
+    random_word = random.choice(list(markov_dict))
+    sentence = random_word
+    value=random_word
+    while (value[-1]!= "." and value[-1]!= "?" and value[-1]!= "!"):
+        if len(value) >1:
+            if ((value[-2]== "." or value[-2]== "?" or value[-2]== "!") and value[-1]== "\""):
+                break
+        new_value = markov_dict.get(value)
+        if len(new_value) > 1:
+            new_value = random.choice(new_value)
+            sentence += " " + new_value
+            value= new_value
+        else:
+            new_value = new_value[0]
+            sentence += " " + new_value
+            value= new_value
+    return sentence
+
+print(generate_sentence())
+print("")
+print(generate_sentence())
+print("")
+print(generate_sentence())
+print("")
+print(generate_sentence())
+print("")
+print(generate_sentence())
--- applications/no_dups/no_dups.py
+++ applications/no_dups/no_dups.py
@@ -1,20 +1,30 @@
+# def no_dups(s):
+#     # Your code here
+#     words = s.split()
+#     no_dups_set=[]
+#     if len(words) < 1:
+#         return ""
+#     for word in words:
+#         if word in no_dups_set:
+#             pass
+#         else:
+#             no_dups_set.append(word)
+#     string = ""
+#     for word in no_dups_set:
+#         string += word + " "
+#     return string.strip()
+
 def no_dups(s):
-    # Your code here
     words = s.split()
-    no_dups_set=[]
+    no_dups_dict = {}
     if len(words) < 1:
         return ""
     for word in words:
-        if word in no_dups_set:
-            pass
-        else:
-            no_dups_set.append(word)
+        no_dups_dict.update({word: True})
     string = ""
-    for word in no_dups_set:
-        string += word + " "
+    for key in no_dups_dict:
+        string += key + " "
     return string.strip()
-
-
 
 if __name__ == "__main__":
     print(no_dups(""))
